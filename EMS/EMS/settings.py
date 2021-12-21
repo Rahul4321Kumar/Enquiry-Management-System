e8c0a805885c6a5612ee2faa41521129886d8fdc
Rahul4321Kumar <rkumar@deqode.com>
2021-12-21 22:35:50 +0530

    changed AUTH_USER_MODEL

--- EMS/EMS/settings.py
+++ EMS/EMS/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'user_app.User'
+AUTH_USER_MODEL = 'users.User'
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
