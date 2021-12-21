ea6e311e420dcbace763b2974b4820ece07db6e9
Rahul4321Kumar <rkumar@deqode.com>
2021-12-21 21:37:30 +0530

    Added 2 line break

--- EMS/enquiries/models.py
+++ EMS/enquiries/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
+
 class Enquiry(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField(max_length=200)
