10ad6929d249f5e4f289cfbb414f8fee8eff9707
Rahul4321Kumar <rkumar@deqode.com>
2021-12-22 18:16:45 +0530

    Fix typo error in created_at field

--- EMS/users/models.py
+++ EMS/users/models.py
@@ -43,7 +43,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     objects = _UserManager()
