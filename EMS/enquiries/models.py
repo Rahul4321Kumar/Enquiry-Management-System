aacb105b10fbf93eb259b2c33e3936e5bce836ca
Rahul4321Kumar <rkumar@deqode.com>
2021-12-22 15:45:19 +0530

    remove comments

--- EMS/enquiries/models.py
+++ EMS/enquiries/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-# Create your models here.
+
 
 class Enquiry(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
