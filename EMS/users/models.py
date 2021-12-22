570ac7019e837700ad751efb33f8d16a47da21a2
Rahul4321Kumar <rkumar@deqode.com>
2021-12-22 17:55:20 +0530

    Add default value to is_staff field

--- EMS/users/models.py
+++ EMS/users/models.py
@@ -42,7 +42,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
     is_superuser = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now=True)
 
