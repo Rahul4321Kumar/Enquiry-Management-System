24d306e709ec02d381c46ead9cd0f3796a1f0962
Rahul4321Kumar <rkumar@deqode.com>
2021-12-24 23:35:34 +0530

    refactor:Change the Enquiry url to enquiry

--- EMS/ems/urls.py
+++ EMS/ems/urls.py
@@ -36,8 +36,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    # path('account/', include('allauth.urls')),
-    # path('accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
     path('admin/', admin.site.urls),
     path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
