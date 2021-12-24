da47f468a74d72a3742da27ba259659112b275b8
Rahul4321Kumar <rkumar@deqode.com>
2021-12-25 00:52:48 +0530

    feat:override email related urls field and swagger url

--- EMS/ems/urls.py
+++ EMS/ems/urls.py
@@ -15,9 +15,40 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ENQUIRY MANAGEMENT API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.ourapp.com/policies/terms/",
+      contact=openapi.Contact(email="contact@enquiries.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/enquiry', include('enquiries.urls')),
     path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls'))
+    path(
+        'auth/registration/account-confirm-email/<str:key>/',
+        ConfirmEmailView.as_view(),
+    ),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path(
+        'auth/account-confirm-email/',
+        VerifyEmailView.as_view(),
+        name='account_email_verification_sent'
+    ),
 ]
