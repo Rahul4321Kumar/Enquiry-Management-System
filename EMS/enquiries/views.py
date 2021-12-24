cfff095494429b52fd301b68367b935ad2d1973c
Rahul4321Kumar <rkumar@deqode.com>
2021-12-25 01:10:16 +0530

    refactor:Add enquiries view where user can list,create,update and delete

--- EMS/enquiries/views.py
+++ EMS/enquiries/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from enquiries.serializers import EnquirySerializer
+from enquiries.models import Enquiry
+from rest_framework.generics import ListCreateAPIView,  RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+
+class EnquiryList(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Enquiry.objects.all()
+    serializer_class = EnquirySerializer
+
+
+class EnquiryDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Enquiry.objects.all()
+    serializer_class = EnquirySerializer
