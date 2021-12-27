59bddbb01127747d73daa6791c92ebe29c12a9a4
Rahul4321Kumar <rkumar@deqode.com>
2021-12-27 08:33:49 +0530

    refactor:Format the code

--- EMS/enquiries/serializers.py
+++ EMS/enquiries/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
+from users.models import User
+
 from enquiries.models import Enquiry
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','email','full_name']
+
 
 class EnquirySerializer(serializers.ModelSerializer):
     class Meta:
         model = Enquiry
-        fields = '__all__'
+        fields = ['id','message']
