8accb9de16844c0950d3b9c5c0c0a7693ee70093
Rahul4321Kumar <rkumar@deqode.com>
2021-12-27 08:43:42 +0530

    refactor:Add re_path to user regular expression in url pattern

--- EMS/ems/urls.py
+++ EMS/ems/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from dj_rest_auth.registration.views import ConfirmEmailView, VerifyEmailView
 from django.contrib import admin
-from django.urls import path, include
-from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
-from drf_yasg.views import get_schema_view
+from django.urls import include, path
+from django.urls.conf import re_path
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -36,10 +36,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/enquiry', include('enquiries.urls')),
+    re_path(r'redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/enquiry/', include('enquiries.urls')),
     path('auth/', include('dj_rest_auth.urls')),
     path(
         'auth/registration/account-confirm-email/<str:key>/',
